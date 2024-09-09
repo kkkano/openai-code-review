127115f00ca07da297345603ca12f3c57c2b940b
wx <657394554@qq.com>
2024-09-10 01:19:57 +0800

    test

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -48,7 +48,7 @@ public class OpenAiCodeReview {
         connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
         connection.setDoOutput(true);
 
-//        String code = "1+1+1";
+//        String code = "1+1+11";
 //
 //        String jsonInpuString = "{"
 //                + "\"model\":\"glm-4-flash\","
