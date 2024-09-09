4a490193b0ff7fff98d2dc9f360e0c8ea9aebe78
wx <657394554@qq.com>
2024-09-10 00:57:12 +0800

    引入chatgml，设计prompt，完成测试

--- docs/curl/curl-glm-4.sh
+++ docs/curl/curl-glm-4.sh
@@ -1,5 +1,5 @@
 curl -X POST \
-        -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsInNpZ25fdHlwZSI6IlNJR04ifQ.eyJhcGlfa2V5IjoiYzc4ZmJhY2QzZTEwMTE4YWQ1NjQ5ZDdhNTRhM2ExNjMiLCJleHAiOjE3MjIwNzczMTQxOTgsInRpbWVzdGFtcCI6MTcyMjA3NTUxNDIxMH0.CdDjJUs8XFUqZnjY08ffaoddLOtoCBn17vmttzkvM0I" \
+        -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsInNpZ25fdHlwZSI6IlNJR04ifQ.eyJhcGlfa2V5IjoiN2VjOTY5ZDUzOTZlMzMxZGFlNjIzODgwZjQ2MzBlODAiLCJleHAiOjE3MjU4NzE3MTkxMTUsInRpbWVzdGFtcCI6MTcyNTg2OTkxOTEzNn0.JzAmAPRTCpujzhwbtelT76OfdpeoALLl-PGjL5iw8bg" \
         -H "Content-Type: application/json" \
         -H "User-Agent: Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)" \
         -d '{
@@ -12,4 +12,5 @@ curl -X POST \
               }
           ]
         }' \
+
   https://open.bigmodel.cn/api/paas/v4/chat/completions
