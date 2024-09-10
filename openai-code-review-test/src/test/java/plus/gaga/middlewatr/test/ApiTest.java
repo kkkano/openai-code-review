901cf11458b1ef2ee8bbcdf842e13cb5aff1e932
wx <657394554@qq.com>
2024-09-11 03:47:57 +0800

    评审消息 微信公众号通知 test 4

--- openai-code-review-test/src/test/java/plus/gaga/middlewatr/test/ApiTest.java
+++ openai-code-review-test/src/test/java/plus/gaga/middlewatr/test/ApiTest.java
@@ -16,7 +16,7 @@ public class ApiTest {
         System.out.println(Integer.parseInt("abc1234"));
         System.out.println(Integer.parseInt("abc12345"));
         System.out.println(Integer.parseInt("abc123456"));
-        System.out.println(Integer.parseInt("abc123456789111111"));
+        System.out.println(Integer.parseInt("abc1234567891101111"));
     }
 
 }
