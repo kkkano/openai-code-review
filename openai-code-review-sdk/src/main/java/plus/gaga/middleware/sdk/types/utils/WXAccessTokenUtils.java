ed23cc80b0cd851fa7fba1d1426c8b8ca285fa9f
wx <657394554@qq.com>
2024-09-11 16:52:06 +0800

    debug

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/types/utils/WXAccessTokenUtils.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/types/utils/WXAccessTokenUtils.java
@@ -13,10 +13,10 @@ public class WXAccessTokenUtils {
     private static final String SECRET = "d7e2beb5c1105aa083d30e6a82eb9d99";
     private static final String GRANT_TYPE = "client_credential";
     private static final String URL_TEMPLATE = "https://api.weixin.qq.com/cgi-bin/token?grant_type=%s&appid=%s&secret=%s";
-    public static String getAccessToken(String APPID,String SECRET) {
+    public static String getAccessToken() {
         return getAccessToken(APPID, SECRET);
     }
-    public static String getAccessToken() {
+    public static String getAccessToken(String APPID, String SECRET) {
         try {
             String urlString = String.format(URL_TEMPLATE, GRANT_TYPE, APPID, SECRET);
             URL url = new URL(urlString);
